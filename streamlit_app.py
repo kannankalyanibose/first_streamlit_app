d094826b5ea678a0a306555bef1ef2309cd5fc0b
kannankalyanibose <123793922+kannankalyanibose@users.noreply.github.com>
2023-01-28 13:16:17 +0530

    Update streamlit_app.py
    Index Test 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,9 +10,9 @@ streamlit.text('🥑 🍞  Avocado Toast')
 streamlit.header(' 🍎 Build your Own Fruit Smoothie 🍇 ')   
 
 my_fruit_list=pa.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-#streamlit.dataframe(my_fruit_list)
+ 
 my_furit_list=my_fruit_list.set_index('Fruit')
 
 #picklist 
-streamlit.multiselect("Pick Your Fruits Here:" , list(my_fruit_list.index))
+streamlit.multiselect("Pick Your Fruits Here1:" , list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
