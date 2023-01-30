5e41cb6a722ab1df02e0013354175f43e6563d3b
kannankalyanibose <123793922+kannankalyanibose@users.noreply.github.com>
2023-01-30 13:49:22 +0530

    Update streamlit_app.py
    final test

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ streamlit.text(' 🐔 Hard-Boilded Free-Range Egg')
 streamlit.text('🥑 🍞  Avocado Toast') 
 streamlit.header(' 🍎 Build your Own Fruit Smoothie 🍇 ')   
 
-my_fruit_list=pa.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#my_fruit_list=pa.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_furit_list=my_fruit_list.set_index('Fruit',inplace=True)
  
 ##picklist 
