dadda36ba39828ccd5321181e5560946b1f5511a
kannankalyanibose <123793922+kannankalyanibose@users.noreply.github.com>
2023-01-30 13:48:24 +0530

    Update streamlit_app.py
    default added

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,6 @@ my_furit_list=my_fruit_list.set_index('Fruit',inplace=True)
 #streamlit.multiselect("Pick Your Fruits Here :" , my_fruit_list.set_index('Serving_Size'))
 streamlit.multiselect("Pick Your Fruits Here :" , list(my_fruit_list.index))
 
-#streamlit.multiselect("Pick Your Fruits Here :" , list(my_fruit_list.index),['Avocado','Strawberries'])
+streamlit.multiselect("Pick Your Fruits Here :" , list(my_fruit_list.index),['Avocado','Strawberries'])
 #stramlit.text(list[my_fruit_list.index])
 streamlit.dataframe(my_fruit_list)
